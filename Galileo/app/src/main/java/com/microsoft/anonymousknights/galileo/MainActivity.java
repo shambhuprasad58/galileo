5d4a283d799b2de7b000ae5030bbea0598a58503
Shambhu Prasad <shambhuprasad58@gmail.com>
2015-07-02 18:25:51 +0530

    basic UI prepared. touch listeners initiated

--- Galileo/app/src/main/java/com/microsoft/anonymousknights/galileo/MainActivity.java
+++ Galileo/app/src/main/java/com/microsoft/anonymousknights/galileo/MainActivity.java
@@ -1,18 +1,59 @@
 package com.microsoft.anonymousknights.galileo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // define the RelativeLayout layout parameters.
+//        RelativeLayout.LayoutParams relativeLayoutParams = new RelativeLayout.LayoutParams(
+//                RelativeLayout.LayoutParams.MATCH_PARENT,
+//                RelativeLayout.LayoutParams.MATCH_PARENT);
+//
+//        setContentView(new GenerateInterface(this).createGridLayout(), relativeLayoutParams);
         setContentView(R.layout.activity_main);
+        LinearLayout baselayout = (LinearLayout) findViewById(R.id.base_layout);
+        baselayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    Log.e("Touch : DOWN",
+                            String.valueOf(motionEvent.getX()) + "x" + String.valueOf(motionEvent.getY()));
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    Log.e("Touch UP: ",
+                            String.valueOf(motionEvent.getX()) + "x" + String.valueOf(motionEvent.getY()));
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_MOVE){
+                    Log.e("Touch MOVE: ",
+                            String.valueOf(motionEvent.getX()) + "x" + String.valueOf(motionEvent.getY()));
+                }
+                return true;
+            }
+        });
+        baselayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("Click: ", "CLICKEDDDDDDDDD");
+            }
+        });
+    }
+
+    public void textviewClick(View v)
+    {
+        Log.e("Click: ",((TextView)v).getText().toString());
     }
 
     @Override
