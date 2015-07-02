1ed7f91c9488501be0862bf32cbeaa0c81c8539c
Arnab Mitra <arnabmitra08@gmail.com>
2015-07-02 19:30:46 +0530

    Dictionary Traversal Untested

--- Galileo/app/src/main/java/T9/T9/T9.java
+++ Galileo/app/src/main/java/T9/T9/T9.java
@@ -1,11 +1,13 @@
 package T9;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
 
 import Contacts.ContactData;
 import Trie.Trie;
 import Trie.Node;
+import Trie.NodeList;
 
 /**
  * Created by Administrator on 7/2/2015.
@@ -36,25 +38,61 @@ public class T9
     }
     public LinkedList<String>[] traverseDictionary()
     {
+        LinkedList<ContactData> masterList = new LinkedList<ContactData>();
         Stack<Integer> stack = new Stack<Integer>();
-        stack.push(0);
+        Stack<Node> nodeStack = new Stack<Node>();
+        Node[] list;
 
         Node current = dictionary.getHead();
         int temp;
-        //TODO: Empty Stack
-        LinkedList<ContactData> list;
+
+        //empty filter results
+        if(current == null)
+        {
+            return null;
+        }
+
+        if(current.getNextChar() > 0)
+        {
+        }
+        else
+        {
+            list = ((NodeList)current.getNext()).getList();
+            for(temp = 0; temp < 12 && list[temp] == null; temp++);
+            if (temp < 12)
+            {
+                stack.push(temp);
+                nodeStack.push(current);
+                current = list[temp];
+            }
+        }
+        nodeStack.push(current);
+
+
         do
         {
             temp = stack.pop();
+            current = nodeStack.pop();
+
+            //add node content
+            masterList.addAll(current.getEntries());
 
             //singleton branch
             if(current.getNextChar() > 0)
             {
                 current = current.getNext();
             }
+            //general branch
             else
             {
-
+                list = ((NodeList)current.getNext()).getList();
+                for(temp++; temp < 12 && list[temp] == null; temp++);
+                if(temp < 12)
+                {
+                    stack.push(temp);
+                    nodeStack.push(current);
+                    current = list[temp];
+                }
             }
         }while(!stack.isEmpty());
 
