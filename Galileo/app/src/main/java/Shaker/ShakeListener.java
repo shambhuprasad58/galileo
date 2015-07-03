bf499e141bc36889d63aecebdb4876db115a5d8e
Shambhu Prasad <shambhuprasad58@gmail.com>
2015-07-03 10:49:31 +0530

    ALL SET

--- Galileo/app/src/main/java/Shaker/ShakeListener.java
+++ Galileo/app/src/main/java/Shaker/ShakeListener.java
@@ -18,7 +18,7 @@ public class ShakeListener implements SensorListener
 {
     private static final int FORCE_THRESHOLD = 350;
     private static final int TIME_THRESHOLD = 100;
-    private static final int SHAKE_TIMEOUT = 500;
+    private static final int SHAKE_TIMEOUT = 300;
     private static final int SHAKE_DURATION = 1000;
     private static final int SHAKE_COUNT = 3;
 
